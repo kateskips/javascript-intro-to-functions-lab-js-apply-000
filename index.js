98f5ed12dab9b0300e6b14018ed717142b6432ad
kateskips <follett.katie@gmail.com>
2018-07-23 21:55:36 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,42 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  if (string === string.toLowerCase()) {
+    var lowercase = string.toLowerCase();
+    return ('I can\'t hear you!'); }
+}
+
+funtion sayHiToGrandma 
+
+
+
+
+
+
+
+
+
+
+  // else if (somethingThatsTrueOrFalse2) {
+  //
+  //} else if (somethingThatsTrueOrFalse3) {
+  //
+  //}
+
+  //return "I can't hear you!";
+
+  //return string.toUpperCase("YES INDEED!")
